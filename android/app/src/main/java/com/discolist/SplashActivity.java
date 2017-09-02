c7adc73858bdc78b696d1317d4a0503ba5989a02
baileya4 <zbailey656@gmail.com>
2017-09-02 12:38:10 -0700

    debugged splash page

--- android/app/src/main/java/com/discolist/SplashActivity.java
+++ android/app/src/main/java/com/discolist/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.discolist;
 
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
 public class SplashActivity extends ReactActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(saveInstanceState);
+        super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
